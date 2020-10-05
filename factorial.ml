63937b144a9f059b3a2dae298901d7ecf30d42ef
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2020-10-05 14:06:46 +0900

    Modified greeting.ml and factorial.ml

--- factorial.ml
+++ factorial.ml
@@ -1,10 +1,12 @@
 open Printf
 
-let rec fact_cps n k =
-  if n = 0 then (k 1)
-  else fact_cps (n - 1) (fun x -> k (n * x))
-
 let fact n =
-  fact_cps n (fun x -> x)
+  let rec aux n k = if n = 0 then (k 1)
+                    else aux (n - 1) (fun x -> k (n * x))
+  in
+  aux n (fun x -> x)
+;;
 
-let () = printf "%d\n" (fact 5);;
+let () =
+  printf "%d\n" (fact 5);
+;;
--- greeting.ml
+++ greeting.ml
@@ -2,6 +2,16 @@ let hello = "Hello, world!!";;
 
 let hello2 name = "Hello, " ^ name ^ "!!";;
 
+let greet name =
+  let s = match name with
+          | None -> "Hello, world!!"
+          | Some name -> "Hello, " ^ name ^ "!!"
+  in
+  print_endline s
+;;
+
 let () =
     print_endline hello;
-    print_endline (hello2 "Tom");;
+    print_endline (hello2 "Tom");
+    greet None;
+    greet (Some "Mike");;
